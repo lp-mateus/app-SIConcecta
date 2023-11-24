4540720eb22eaa446a33fd2a17147b8f933af367
mateus.pedrosa <mateus.pedrosa@senaicni.com.br>
2023-11-24 15:42:17 -0300

    first commit

---
+++ advancedConfigs.js
@@ -0,0 +1,12 @@
+"locale: 'pt-BR',
+height: '100%',
+titleFormat: { month: 'long' },
+dayHeaderFormat: { weekday: 'short' },
+fixedWeekCount: false,
+showNonCurrentDates: false,
+navLinks: true, 
+headerToolbar: {
+    start: 'title', 
+    center: '',
+    end: 'today,dayGridMonth prev,next' 
+}"
