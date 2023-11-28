65445b55b38e0506cf37584f55babda295362fe6
mateus.pedrosa <mateus.pedrosa@senaicni.com.br>
2023-11-28 18:41:01 -0300

    feat: style events

--- advancedConfigs.js
+++ advancedConfigs.js
@@ -5,6 +5,11 @@ dayHeaderFormat: { weekday: 'short' },
 fixedWeekCount: false,
 showNonCurrentDates: false,
 navLinks: true, 
+eventTimeFormat: {
+    hour: '2-digit',
+    minute: '2-digit',
+    omitZeroMinute: false,
+},
 headerToolbar: {
     start: 'title', 
     center: '',
